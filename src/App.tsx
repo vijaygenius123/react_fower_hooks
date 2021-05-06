0e14e8836ecf9fc08a88c360ff4d5b64e6cedd70
Vijayaraghavan Sundararaman <Vijayaraghavan_Sundararaman@amat.com>
2021-05-06 15:47:08 +0530

    feat(app.tsx): allow user to search and display pokemons
    Pokemons are loaded from a json file. When the component is mounted we will load all the pokemons
    into the state and display the top 10 results

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Box } from '@fower/react'
 import { styled } from '@fower/styled'
-
 
 interface Pokemon {
     id: number,
@@ -18,9 +17,19 @@ const Input = styled("input")
 function App() {
 
     const [filter, setFilter] = useState('')
-    const [allPokemon, setAllPokemon] = useState([])
+    const [allPokemon, setAllPokemon] = useState<Pokemon[]>([])
 
 
+    useEffect(function (){
+        fetch('/pokemon.json')
+            .then(resp => resp.json())
+            .then((pokemon: Pokemon[]) => setAllPokemon(pokemon))
+    },[])
+
+    const lowerCaseFilter = filter.toLowerCase()
+    const pokemon = allPokemon
+        .filter(({name: {english}}) => english.toLowerCase().includes(lowerCaseFilter))
+        .slice(0, 10)
 
     return (
      <Box p-10 maxW-1200 m="auto">
@@ -33,7 +42,11 @@ function App() {
             value={filter}
             onChange={(evt) => setFilter(evt.target.value)}
         />
-         {filter}
+         {pokemon.map(pokemon => (
+             <Box key={pokemon.id} text2XL >
+                 {pokemon.name.english}
+             </Box>
+         ))}
      </Box>
   );
 }
